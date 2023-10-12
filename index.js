b3be70e9819f71b4cd1ff8110105f1f881fcaa2a
James 'Tucker' Wray <tuckerwray84@gmail.com>
2023-10-12 15:15:28 -0400

    Update index.js
    converting into an installable npm CLI. stepping away from the prompting user for the repourl to accept a command line arg for the url from user.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
+#!/usr/bin/env node
+
 const open = require('open');
-const readline = require('readline');
-
-const promptSTRING = "\n1: open repo in chrome \n2: navigate down to directory containing package.json \n3: copy paste URL at that level. \n4: Paste it here \n5: press ENTER \n"
-
-const readlineInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
 
 async function generateCodeSandboxUrl(githubURL) {
     // Extract username and repository name from the GitHub URL
@@ -20,11 +14,6 @@ async function generateCodeSandboxUrl(githubURL) {
     }
 }
 
-readlineInterface.question(promptSTRING, githubURL => {
-    generateCodeSandboxUrl(githubURL);
-    readlineInterface.close();
-});
-
-// code for a cli
-// example input: :https://github.com/aymanfarhat/js13k-starter"
-// expected output : "https://codesandbox.io/p/github/aymanfarhat/js13k-starter"opened in new browser window
+// Accept GitHub URL as a command-line argument
+const githubURL = process.argv[2];
+generateCodeSandboxUrl(githubURL);
